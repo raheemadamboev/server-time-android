670c4b10264323bfe1f3e4aa2979fbce9492f5be
Raheem <raheemideadev@gmail.com>
2021-10-07 00:17:03 +0500

    demo app

--- app/src/main/java/xyz/teamgravity/servertimedemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/servertimedemo/MainActivity.kt
@@ -1,8 +1,10 @@
 package xyz.teamgravity.servertimedemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import xyz.teamgravity.servertime.ServerTime
 import xyz.teamgravity.servertimedemo.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        button()
+    }
+
+    private fun button() {
+        onGetTime()
+    }
+
+    private fun onGetTime() {
+        binding.apply {
+            timeB.setOnClickListener {
+                timeT.text = getString(R.string.retrieving_time)
+
+                ServerTime().execute { time ->
+                    when (time) {
+
+                        ServerTime.UNKNOWN_HOST -> { // internet not working or error in host
+                            timeT.text = getString(R.string.unknown_host)
+                        }
+
+                        ServerTime.IO_EXCEPTION -> { // io exception
+                            timeT.text = getString(R.string.io_exception)
+                        }
+
+                        else -> { // we got time successfully
+                            timeT.text = Date(time).toString()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
