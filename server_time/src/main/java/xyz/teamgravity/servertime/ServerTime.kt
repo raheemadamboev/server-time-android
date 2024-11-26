89012a2882fec16ab2bebc56ed3aeb82ecc34faf
raheem <raheemideadev@gmail.com>
2024-11-26 19:54:14 +0500

    [1.1.1] Upgraded dependencies, Kotlin, Gradle and targetSdk, compileSdk;

--- server_time/src/main/java/xyz/teamgravity/servertime/ServerTime.kt
+++ server_time/src/main/java/xyz/teamgravity/servertime/ServerTime.kt
@@ -1,6 +1,10 @@
 package xyz.teamgravity.servertime
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.apache.commons.net.ntp.NTPUDPClient
 import java.io.IOException
 import java.net.InetAddress
@@ -8,57 +12,45 @@ import java.net.UnknownHostException
 
 class ServerTime {
 
-    companion object {
-        private const val SERVER_ADDRESS = "time.google.com"
-        private const val TIMEOUT = 5_000L
-
-        const val UNKNOWN_HOST = -1L
-        const val IO_EXCEPTION = -2L
-        const val TIMEOUT_EXCEPTION = -3L
+    private companion object {
+        const val SERVER_ADDRESS = "time.google.com"
     }
 
+    private val scope: CoroutineScope = MainScope()
+
+    ///////////////////////////////////////////////////////////////////////////
+    // API
+    ///////////////////////////////////////////////////////////////////////////
 
     fun execute(listener: (time: Long) -> Unit) {
-        CoroutineScope(Dispatchers.IO).launch(Dispatchers.IO) {
-            withTimeoutOrNull(TIMEOUT) {
-                withContext(Dispatchers.IO) {
-                    var client: NTPUDPClient? = null
-                    try {
-                        client = NTPUDPClient()
-                        val address: InetAddress = InetAddress.getByName(SERVER_ADDRESS)
-                        val info= client.getTime(address)
-                        val time = info.message.transmitTimeStamp.time
-
-                        withContext(Dispatchers.Main) { listener(time) }
-                    } catch (e: UnknownHostException) {
-                        withContext(Dispatchers.Main) { listener(UNKNOWN_HOST) }
-                    } catch (e: IOException) {
-                        withContext(Dispatchers.Main) { listener(IO_EXCEPTION) }
-                    } finally {
-                        client?.close()
-                    }
-                }
-            } ?: withContext(Dispatchers.Main) { listener(TIMEOUT_EXCEPTION) }
+        scope.launch {
+            listener(execute())
         }
     }
 
     suspend fun execute(): Long {
-        return withTimeoutOrNull(TIMEOUT) {
-            withContext(Dispatchers.IO) {
-                var client: NTPUDPClient? = null
-                try {
-                    client = NTPUDPClient()
-                    val address: InetAddress = InetAddress.getByName(SERVER_ADDRESS)
-                    val info= client.getTime(address)
-                    return@withContext info.message.transmitTimeStamp.time
-                } catch (e: UnknownHostException) {
-                    return@withContext UNKNOWN_HOST
-                } catch (e: IOException) {
-                    return@withContext IO_EXCEPTION
-                } finally {
-                    client?.close()
-                }
+        return withContext(Dispatchers.IO) {
+            val client = NTPUDPClient()
+            try {
+                val address: InetAddress = InetAddress.getByName(SERVER_ADDRESS)
+                val info = client.getTime(address)
+                return@withContext info.message.transmitTimeStamp.time
+            } catch (e: UnknownHostException) {
+                return@withContext Result.UNKNOWN_HOST
+            } catch (e: IOException) {
+                return@withContext Result.IO_EXCEPTION
+            } finally {
+                client.close()
             }
-        } ?: TIMEOUT_EXCEPTION
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // MISC
+    ///////////////////////////////////////////////////////////////////////////
+
+    object Result {
+        const val UNKNOWN_HOST = -1L
+        const val IO_EXCEPTION = -2L
     }
 }
